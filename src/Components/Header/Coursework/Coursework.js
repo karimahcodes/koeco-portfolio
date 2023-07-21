e799e21298b6e69a30778f35bc0cbecd2c9f4905
Karimah Nichols <ms.karimah.nichols@gmail.com>
2023-07-21 14:09:18 -0400

    update variables, technologies div, experiement with background elements. Add component for coursework.

---
+++ src/Components/Header/Coursework/Coursework.js
@@ -0,0 +1,14 @@
+import './Coursework.scss'
+
+export default function Coursework() {
+
+
+
+    return(
+        <div className='coursework'>
+
+        <image className='coursework__image' source="" alt=""></image>
+
+        </div>
+    )
+}
--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -7,15 +7,27 @@ export default function HomePage() {
     return (
         <div className='home'>
 
-            <nav>
-                <a className='home__subnav' href="#contact">Contact</a>
-                <a className='home__subnav' href="#technologies">Technologies</a>
+            <nav className='home__subnav'>
+                <a className='home__subnav-link' href="#contact">
+                    <p className='home__subnav-text'> Contact</p>
+                </a>
+
+                <a className='home__subnav-link' href="#technologies">
+                    <p className='home__subnav-text'>Technologies</p>
+                </a>
             </nav>
 
+           
             <section className='home__about'>
-                <h2 className='home__title'>About me</h2>
+                <h2 className='home__title'>About Me</h2>
                 <p className='home__body-text'> koeco reflects my passion for using tech to build community and find creative new tools to meet the needs of those not traditionally courted by the tech industry. Prior to koeco, I worked with behavioral healthcare, nonprofit and social change organizations. My preferred style of working on a project blends compassion, collaboration, and client-driven solutions with code. I can't wait to work with you!</p>
+
             </section>
+
+            <div className='home__about--image'>
+                TEST
+            </div>
+           
 
             <section className='home__contact' id="contact">
                 <h2 className='home__title'>Contact Me</h2>
@@ -25,13 +37,13 @@ export default function HomePage() {
                     <input className='home__contact-name' type="text" id="contact-name" placeholder='Toni Braxton (she/her)' />
 
                     <label className="home__label" htmlFor='email-input'>Email</label>
-                    <input className='home__email' type="text" id="email-input" placeholder="you@mail.com" />
+                    <input className='home__email' type="email" id="email-input" placeholder="you@mail.com" />
 
                     <label className="home__label" htmlFor='message'>What brings you here today?</label>
                     <textarea className='home__message' placeholder='message' id="message" />
 
                     <div className='home__btn-group'>
-                        <button className="home__button" type="submit">Submit</button> 
+                        <button className="home__button" type="submit">Submit</button>
                         <button className="home__button" type="submit">Clear All</button>
                     </div>
                 </form>
@@ -39,17 +51,31 @@ export default function HomePage() {
 
             </section>
 
-            <section className='home__about' id="technologies">
-                <h2 className='home__title'>Technologies</h2>
+            <section className='home__technologies' id="technologies">
+                <h2 className='home__title'>Technologies & Skills</h2>
 
-                <ul className="tech__list">
-                    <li className="tech__list-item">HTML</li>
-                    <li className="tech__list-item">CSS</li>
-                    <li className="tech__list-item">JavaScript</li>
-                    <li className="tech__list-item">React</li>
-                    <li className="tech__list-item">Node</li>
-                    <li className="tech__list-item">Express</li>
-                    <li className="tech__list-item">MySQL</li>
+                <ul className="home__list">
+                    <div className='home__frontend'>
+                        <li className="home__list-item">HTML5</li>
+                        <li className="home__list-item">CSS3</li>
+                        <li className="home__list-item">JavaScript</li>
+                        <li className="home__list-item">React</li>
+                    </div>
+                    <div className='home__backend'>
+                        <li className="home__list-item">Node</li>
+                        <li className="home__list-item">Express</li>
+                        <li className="home__list-item">MySQL</li>
+                    </div>
+                    <div className='home__nocode'>No Code:
+                        <li className="home__list-item">Wix</li>
+                        <li className="home__list-item">Squarespace</li>
+                    </div>
+                    <div className='home__transferrable'>Non-technical  Skills:
+                        <li className="home__list-item">Cross-cultural & Interdisciplinary Collaboration</li>
+                        
+                        <li className="home__list-item">Intermediate Spanish</li>
+                    </div>
+
                 </ul>
             </section>
 
